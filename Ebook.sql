0516ffd743b79c6adbb5d4fe9aa69af7f0616ed5
ppecheux <ppecheux@etu.utc.fr>
2018-05-15 16:12:08 +0200

    trois tables

--- Ebook.sql
+++ Ebook.sql
@@ -1,7 +1,19 @@
 CREATE TABLE Categorie (
-    nom VARCHAR PRIMARY KEY,
-    description VARCHAR ); 
-    
+    nom VARCHAR(255) PRIMARY KEY,
+    description VARCHAR );
+
 CREATE TABLE Licence (
-    
-)
+  nom VARCHAR(255) PRIMARY KEY,
+  droitModification BOOLEAN NOT NULL ,
+  partageMemeCondition BOOLEAN NOT NULL ,
+  droitUtilisationCommercial BOOLEAN NOT NULL ,
+  UNIQUE (droitModification,partageMemeCondition,droitUtilisationCommercial)
+);
+
+CREATE TABLE Auteur (
+  nom VARCHAR(255),
+  prenom VARCHAR(255),
+  biographie VARCHAR(255) UNIQUE,
+  nationalite VARCHAR(255),
+  PRIMARY KEY (nom,prenom)
+);
