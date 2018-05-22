54c24c11c4b470cabe6e11e7593d59ead49442b2
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2018-05-22 14:32:32 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -44,6 +44,7 @@ CREATE TABLE Aime (
     utilisateur VARCHAR(255) REFERENCES UtilisateursEnregistres(email),
     titre VARCHAR(255),
     langue VARCHAR(255),
+    PRIMARY KEY(utilisateur,titre,langue),
     FOREIGN KEY (titre,langue) REFERENCES Livre(langue)
 );
 
@@ -52,6 +53,17 @@ CREATE TABLE Telechargement (
     titre VARCHAR(255),
     langue VARCHAR(255),
     prixAchat FLOAT,
+    PRIMARY KEY(utilisateur,titre,langue),
     FOREIGN KEY (titre,langue) REFERENCES Livre(langue)
+);
+
+CREATE TABLE Ecrire (
+    auteurNom VARCHAR(255),
+    auteurPrenom VARCHAR(255),
+    titre VARCHAR(255),
+    langue VARCHAR(255),
+    PRIMARY KEY (auteurNom,auteurPrenom,titre,langue),
+    FOREIGN KEY(auteurNom,auteurPrenom) REFERENCES Auteur(prenom,nom),
+    FOREIGN KEY(titre,langue) REFERENCES Livre(titre,langue)
 );
 
