2196ea72f65336d108495e1034cebf597c0c7996
Pierre-Louis Pecheux <pierre-louis.pecheux@etu.utc.fr>
2018-05-22 14:11:45 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -17,3 +17,35 @@ CREATE TABLE Auteur (
   nationalite VARCHAR(255),
   PRIMARY KEY (nom,prenom)
 );
+
+CREATE TABLE UtilisateursEnregistres (
+    email VARCHAR(255) PRIMARY KEY,
+    motDePasse VARCHAR(255),
+    nom VARCHAR(255),
+    prenom VARCHAR(255)
+);
+
+CREATE TABLE Don (
+    montantDon FLOAT,
+    dateDon DATE,
+    utilisateur VARCHAR(255) REFERENCES UtilisateursEnregistres(email),
+     PRIMARY KEY (montantDon,dateDon,utilisateur)
+);
+
+CREATE TABLE Abonnement (
+    auteurNom VARCHAR(255),
+    auteurPrenom VARCHAR(255),
+    utilisateur VARCHAR(255) REFERENCES UtilisateursEnregistres(email),
+    PRIMARY KEY (auteurNom,auteurPrenom,utilisateur),
+    FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom)
+);
+
+CREATE TABLE Reference (
+    auteurNom VARCHAR(255) REFERENCES Auteur(nom),
+    auteurPrenom VARCHAR(255) REFERENCES Auteur(prenom),
+    livre VARCHAR(255) REFERENCES Livre(titre),
+    langue VARCHAR(255) REFERENCES Livre(langue),
+    PRIMARY KEY (auteurNom,auteurPrenom,livre, langue),
+    FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom),
+    FOREIGN KEY (livre,langue) REFERENCES Livre(nom,prenom),
+);
