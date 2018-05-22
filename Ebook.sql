79659ad177de26810ba711bcc0e43acecf2a0a84
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2018-05-22 16:13:12 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -1,4 +1,6 @@
-﻿DROP TABLE Reference; 
+﻿DROP VIEW vVedette;
+DROP TABLE Ecrire;
+DROP TABLE Reference; 
 DROP TABLE Telechargement;
 DROP TABLE Aime;  
 DROP TABLE Abonnement; 
@@ -6,6 +8,8 @@ DROP TABLE Don;
 DROP TABLE UtilisateursEnregistres; 
 DROP TABLE Vedette; 
 DROP TABLE Livre;
+DROP TABLE Citoyen; 
+DROP TABLE Nationalite; 
 DROP TABLE Auteur;
 DROP TABLE Licence;
 DROP TABLE Categorie;
@@ -15,28 +19,51 @@ CREATE TABLE Categorie (
     description VARCHAR );
 
 
+CREATE TABLE Licence (
+  id INTEGER PRIMARY KEY,
+  droitModification BOOLEAN NOT NULL ,
+  partageMemeCondition BOOLEAN NOT NULL ,
+  droitUtilisationCommercial BOOLEAN NOT NULL ,
+  UNIQUE (droitModification,partageMemeCondition,droitUtilisationCommercial)
+);
+
 CREATE TABLE Auteur (
   nom VARCHAR(255),
   prenom VARCHAR(255),
   biographie VARCHAR(255) UNIQUE,
-  nationalite VARCHAR(255),
   PRIMARY KEY (nom,prenom)
 );
+
+CREATE TABLE Nationalite (
+  pays VARCHAR(255)
+);
+
+CREATE TABLE Citoyen (
+  pays VARCHAR(255), 
+  auteurNom VARCHAR(255), 
+  auteurPrenom VARCHAR(255),
+  FOREIGN KEY(pays) REFERENCES Nationalite(pays), 
+  FOREIGN KEY(auteurNom, auteurPrenom) REFERENCES Auteur(nom, prenom),
+  PRIMARY KEY(pays, auteurNom, auteurPrenom)
+);
+
 
 CREATE TABLE Livre (
   titre VARCHAR(255), 
   langue VARCHAR(255), 
-  DatePublication DATE, 
+  page INTEGER NOT NULL, 
+  resume VARCHAR(255), 
+  datePublication DATE, 
   categorie VARCHAR(255) NOT NULL, 
-  licence VARCHAR(255) NOT NULL, 
+  licence INTEGER NOT NULL, 
   FOREIGN KEY(categorie) REFERENCES Categorie(nom), 
-  FOREIGN KEY(licence) REFERENCES Licence(nom), 
+  FOREIGN KEY(licence) REFERENCES Licence(id), 
   PRIMARY KEY(titre, langue)
 ); 
 
 CREATE TABLE Vedette (
-  DateLimite DATE, 
-  PhraseAccroche VARCHAR(255), 
+  dateLimite DATE, 
+  phraseAccroche VARCHAR(255), 
   titre VARCHAR(255),
   langue VARCHAR(255), 
   PRIMARY KEY (DateLimite, PhraseAccroche, titre, langue), 
@@ -92,14 +119,24 @@ CREATE TABLE Reference (
     FOREIGN KEY (titre,langue) REFERENCES Livre(titre,langue)
 );
 
-CREATE VIEW Vedette AS
+CREATE TABLE Ecrire ( 
+    auteurNom VARCHAR(255), 
+    auteurPrenom VARCHAR(255), 
+    titre VARCHAR(255), 
+    langue VARCHAR(255), 
+    PRIMARY KEY (auteurNom,auteurPrenom,titre,langue), 
+    FOREIGN KEY(auteurNom,auteurPrenom) REFERENCES Auteur(prenom,nom), 
+    FOREIGN KEY(titre,langue) REFERENCES Livre(titre,langue) 
+); 
+
+CREATE VIEW vVedette AS
 SELECT titre,langue,phraseAccroche,page,resume,datePublication,categorie,licence
-FROM (
-    SELECT *
+FROM(
+    SELECT Livre.titre, Livre.langue, Livre.datePublication, Livre.page, Livre.resume, Livre.categorie, Livre.licence, Vedette.dateLimite, Vedette.phraseAccroche
     FROM Livre,Vedette
     WHERE (Livre.titre=Vedette.titre) AND (Livre.langue=Vedette.langue)
-)
-WHERE dateLimite>curdate();
+) AS sousRequete
+WHERE sousRequete.dateLimite>DATE(NOW());
     
 INSERT INTO Categorie (nom, description)
 VALUES ('Fiction','Aventure & Action Classiques Erotique Espionnage Fantastique Frisson & Terreur');
@@ -110,3 +147,24 @@ VALUES ('Bande Dessinée','Aventure Classiques Fantastique Heroïc Fantasy');
 
 INSERT INTO Categorie (nom, description)
 VALUES ('Culture','Arts généraux Architecture Cinéma Cinéma - Scénarios');
+
+
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (1,TRUE,FALSE,TRUE);
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (2,TRUE,TRUE,TRUE);
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (3,FALSE,FALSE,TRUE);
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (4,TRUE,FALSE,FALSE);
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (5,TRUE,TRUE,FALSE);
+INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
+VALUES (6,FALSE,TRUE,FALSE);
+
+INSERT INTO Auteur (nom, prenom, biographie, nationalite)
+VALUES ('Kevin','Integration', 'integre les étudiants en automne et au printemps','France');
+INSERT INTO Auteur (nom, prenom )
+VALUES ('Kevin','Olivera');
+INSERT INTO Auteur (nom,prenom,nationalite )
+VALUES ('Dominique','DesMaths', 'GR');
