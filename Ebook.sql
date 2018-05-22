858347ef1d54dfc70d5afafe7c6de98268ec5b85
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2018-05-22 14:19:37 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -77,9 +77,9 @@ CREATE TABLE Telechargement (
 CREATE TABLE Reference (
     auteurNom VARCHAR(255) REFERENCES Auteur(nom),
     auteurPrenom VARCHAR(255) REFERENCES Auteur(prenom),
-    livre VARCHAR(255) REFERENCES Livre(titre),
-    langue VARCHAR(255) REFERENCES Livre(langue),
+    titre VARCHAR(255),
+    langue VARCHAR(255),
     FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom),
     PRIMARY KEY (auteurNom,auteurPrenom,livre, langue),
-    FOREIGN KEY (livre,langue) REFERENCES Livre(nom,prenom),
+    FOREIGN KEY (titre,langue) REFERENCES Livre(titre,langue),
 );
