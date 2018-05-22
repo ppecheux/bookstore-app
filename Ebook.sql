7be48b5ac65cad001f0eb80be4420e58cd0cc121
Pierre-Louis Pecheux <pierre-louis.pecheux@etu.utc.fr>
2018-05-22 14:34:25 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -1,51 +1,12 @@
 CREATE TABLE Categorie (
     nom VARCHAR(255) PRIMARY KEY,
     description VARCHAR );
+    
+INSERT INTO Categorie (nom, description)
+VALUES ('Fiction','Aventure & Action Classiques Erotique Espionnage Fantastique Frisson & Terreur');
 
-CREATE TABLE Licence (
-  nom VARCHAR(255) PRIMARY KEY,
-  droitModification BOOLEAN NOT NULL ,
-  partageMemeCondition BOOLEAN NOT NULL ,
-  droitUtilisationCommercial BOOLEAN NOT NULL ,
-  UNIQUE (droitModification,partageMemeCondition,droitUtilisationCommercial)
-);
+INSERT INTO Categorie (nom, description)
+VALUES ('Bande Dessinée','Aventure Classiques Fantastique Heroïc Fantasy');
 
-CREATE TABLE Auteur (
-  nom VARCHAR(255),
-  prenom VARCHAR(255),
-  biographie VARCHAR(255) UNIQUE,
-  nationalite VARCHAR(255),
-  PRIMARY KEY (nom,prenom)
-);
-
-CREATE TABLE UtilisateursEnregistres (
-    email VARCHAR(255) PRIMARY KEY,
-    motDePasse VARCHAR(255),
-    nom VARCHAR(255),
-    prenom VARCHAR(255)
-);
-
-CREATE TABLE Don (
-    montantDon FLOAT,
-    dateDon DATE,
-    utilisateur VARCHAR(255) REFERENCES UtilisateursEnregistres(email),
-     PRIMARY KEY (montantDon,dateDon,utilisateur)
-);
-
-CREATE TABLE Abonnement (
-    auteurNom VARCHAR(255),
-    auteurPrenom VARCHAR(255),
-    utilisateur VARCHAR(255) REFERENCES UtilisateursEnregistres(email),
-    PRIMARY KEY (auteurNom,auteurPrenom,utilisateur),
-    FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom)
-);
-
-CREATE TABLE Reference (
-    auteurNom VARCHAR(255) REFERENCES Auteur(nom),
-    auteurPrenom VARCHAR(255) REFERENCES Auteur(prenom),
-    titre VARCHAR(255) REFERENCES Livre(titre),
-    langue VARCHAR(255) REFERENCES Livre(langue),
-    PRIMARY KEY (auteurNom,auteurPrenom,livre, langue),
-    FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom),
-    FOREIGN KEY (livre,langue) REFERENCES Livre(titre,langue),
-);
+INSERT INTO Categorie (nom, description)
+VALUES ('Culture','Arts généraux Architecture Cinéma Cinéma - Scénarios');
