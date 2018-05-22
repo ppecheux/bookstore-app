e3fc1a783775146262e1655dc2f89f0991e8d5dd
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2018-05-22 14:10:38 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -58,22 +58,3 @@ CREATE TABLE Abonnement (
     PRIMARY KEY (auteurNom,auteurPrenom,utilisateur),
     FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom)
 );
-CREATE TABLE Livre (
-  titre VARCHAR(255), 
-  langue VARCHAR(255), 
-  DatePublication DATE, 
-  categorie VARCHAR(255) NOT NULL, 
-  licence VARCHAR(255) NOT NULL, 
-  FOREIGN KEY(categorie) REFERENCES Categorie(nom), 
-  FOREIGN KEY(licence) REFERENCES Licence(nom), 
-  PRIMARY KEY(titre, langue)
-); 
-
-CREATE TABLE Vedette (
-  DateLimite DATE, 
-  PhraseAccroche VARCHAR(255), 
-  titre VARCHAR(255),
-  langue VARCHAR(255), 
-  PRIMARY KEY (DateLimite, PhraseAccroche, titre, langue), 
-  FOREIGN KEY (titre, langue) REFERENCES Livre(titre, langue)
-);
