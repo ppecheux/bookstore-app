8d3df846a5694d02df053918a90ea021997fe127
Simeon Loiseaux <simeon.loiseaux@etu.utc.fr>
2018-05-26 16:54:00 +0200

    Update Ebook.sql

--- Ebook.sql
+++ Ebook.sql
@@ -243,3 +243,26 @@ VALUES ('Kevin', 'Integration', 'plaquette de l integration', 'français');
 INSERT INTO Ecrire (auteurNom, auteurPrenom, titre, langue)
 VALUES ('Dominique', 'DesMaths', 'maths for all', 'anglais'); 
 
+/* REQUETES FONCTIONS */
+
+/* un utilisateur peut afficher l’ensemble des livres référencés sur le site */
+
+SELECT *
+FROM Livre;
+
+/* un utilisateur peut rechercher un livre par auteur ou par titre (ou les 2) */
+
+SELECT *
+FROM Livre, Ecrire
+WHERE Livre.titre=Ecrire.titre
+AND Livre.titre LIKE '%titreVoulu%'
+AND auteurnom LIKE '%nomVoulu%'
+AND auteurprenom LIKE '%prenomVoulu%';
+
+/* un utilisateur peut effectuer une recherche dans les livres d’une catégorie seulement */
+
+SELECT *
+FROM Livre
+WHERE categorie='categorieVoulue';
+
+
