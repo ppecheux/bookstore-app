c3bbd930e33335a7c2a5711dd8f21176f5631c6e
Pierre-Louis Pecheux <pierre-louis.pecheux@etu.utc.fr>
2018-06-11 10:25:09 +0200

    Revert "Merge branch 'pl' into 'master'"
    This reverts commit e5ff791294186859b75efeaab451d74d6164889e

--- Ebook.sql
+++ Ebook.sql
@@ -20,17 +20,6 @@ CREATE TABLE Categorie (
     nom VARCHAR(255) PRIMARY KEY,
     description VARCHAR );
 
-INSERT INTO Categorie (nom, description)
-VALUES ('Fiction','Aventure & Action Classiques Erotique Espionnage Fantastique Frisson & Terreur');
-INSERT INTO Categorie (nom, description)
-VALUES ('Bande Dessinée','Aventure Classiques Fantastique Heroïc Fantasy');
-INSERT INTO Categorie (nom, description)
-VALUES ('Culture','Arts généraux Architecture Cinéma Cinéma - Scénarios');
-INSERT INTO Categorie (nom)
-VALUES ('Musique');
-INSERT INTO Categorie (nom)
-VALUES ('Film');
-
 
 CREATE TABLE Licence (
   id INTEGER PRIMARY KEY,
@@ -40,19 +29,6 @@ CREATE TABLE Licence (
   UNIQUE (droitModification,partageMemeCondition,droitUtilisationCommercial)
 );
 
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (1,TRUE,FALSE,TRUE);
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (2,TRUE,TRUE,TRUE);
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (3,FALSE,FALSE,TRUE);
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (4,TRUE,FALSE,FALSE);
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (5,TRUE,TRUE,FALSE);
-INSERT INTO Licence (id, droitModification, partageMemeCondition, droitUtilisationCommercial)
-VALUES (6,FALSE,TRUE,FALSE);
-
 CREATE TABLE Auteur (
   nom VARCHAR(255),
   prenom VARCHAR(255),
@@ -60,27 +36,10 @@ CREATE TABLE Auteur (
   PRIMARY KEY (nom,prenom)
 );
 
-INSERT INTO Auteur (nom, prenom, biographie)
-VALUES ('Kevin','Integration', 'integre les étudiants en automne et au printemps');
-INSERT INTO Auteur (nom, prenom )
-VALUES ('Kevin','Olivera');
-INSERT INTO Auteur (nom,prenom )
-VALUES ('Dominique','DesMaths');
-
 CREATE TABLE Nationalite (
   pays VARCHAR(255) PRIMARY KEY
 );
 
-INSERT INTO Nationalite (pays) VALUES ('Pays-Bas');
-INSERT INTO Nationalite (pays) VALUES ('Norvège');
-INSERT INTO Nationalite (pays) VALUES ('Biélorussie');
-INSERT INTO Nationalite (pays) VALUES ('Belgique');
-INSERT INTO Nationalite (pays) VALUES ('Azerbaïdjan');
-INSERT INTO Nationalite (pays) VALUES ('Arménie');
-INSERT INTO Nationalite (pays) VALUES ('Autriche');
-INSERT INTO Nationalite (pays) VALUES ('Andorre');
-INSERT INTO Nationalite (pays) VALUES ('Albanie');
-INSERT INTO Nationalite (pays) VALUES ('Allemagne');
 
 CREATE TABLE Citoyen (
   pays VARCHAR(255), 
@@ -91,45 +50,19 @@ CREATE TABLE Citoyen (
   PRIMARY KEY(pays, auteurNom, auteurPrenom)
 );
 
-INSERT INTO Citoyen (pays,auteurNom,auteurPrenom) VALUES ('Allemagne','Kevin','Integration');
-INSERT INTO Citoyen (pays,auteurNom,auteurPrenom) VALUES ('Albanie','Kevin','Integration');
-INSERT INTO Citoyen (pays,auteurNom,auteurPrenom) VALUES ('Albanie','Kevin','Olivera');
-INSERT INTO Citoyen (pays,auteurNom,auteurPrenom) VALUES ('Azerbaïdjan','Kevin','Integration');
 
 CREATE TABLE Livre (
   titre VARCHAR(255), 
   langue VARCHAR(255), 
-  page INTEGER NOT NULL,
+  page INTEGER NOT NULL, 
   resume VARCHAR(255), 
   datePublication DATE, 
   categorie VARCHAR(255) NOT NULL, 
   licence INTEGER NOT NULL, 
   FOREIGN KEY(categorie) REFERENCES Categorie(nom), 
   FOREIGN KEY(licence) REFERENCES Licence(id), 
-  PRIMARY KEY(titre, langue),
-  CHECK (page>0)
+  PRIMARY KEY(titre, langue)
 ); 
-
-/*INSERT INTO Livre (titre,langue,page,resume,datePublication,categorie,licence) 
-VALUES ('Yeux Revolver','France',-3,
-'Était si différent de toi Quand jétais enfant, mon prince charmant Était bien autrement',
-'2000/01/13','Musique', 1); viole la contrainte de vérification « livre » de la relation « livre_page_check » */
-INSERT INTO Livre (titre,langue,page,resume,datePublication,categorie,licence) 
-VALUES ('Yeux Revolver','France',3,
-'Était si différent de toi Quand jétais enfant, mon prince charmant Était bien autrement',
-'2000/01/13','Musique', 1);
-INSERT INTO Livre (titre,langue,page,resume,datePublication,categorie,licence) 
-VALUES ('Ce soir je ne dors pas','France',3,
-'Était si différent de toi Quand jétais enfant, mon prince charmant Était bien autrement',
-'2000/01/13','Musique', 1);
-INSERT INTO Livre (titre,langue,page,resume,datePublication,categorie,licence) 
-VALUES ('Ce soir je ne dors pas','Allemand',3,
-'Était si différent de toi Quand jétais enfant, mon prince charmant Était bien autrement',
-'2000/01/13','Musique', 1);
-VALUES ('Ce soir je ne dors pas','Italien',3,
-'Était si différent de toi Quand jétais enfant, mon prince charmant Était bien autrement',
-'2000/01/13','Musique', 9);
-
 
 
 CREATE TABLE Vedette (
@@ -150,20 +83,6 @@ CREATE TABLE UtilisateursEnregistres (
 );
 
 
-INSERT INTO UtilisateursEnregistres (nom, prenom, motDePasse, email)
-VALUES ('Kevin','Integration', 'pcqeudçjdé','cenestpasuneaddresse');
-INSERT INTO UtilisateursEnregistres (nom, prenom, motDePasse, email)
-VALUES ('Kevin','Integration', 'pcqeudçjdé','adresse@etu.utc.fr');
-INSERT INTO UtilisateursEnregistres (email , motDePasse, prenom)
-VALUES ('cettePersonneNaPasDAdresse','passe','Jose');
-INSERT INTO UtilisateursEnregistres (email , motDePasse, prenom)
-VALUES ('Ron@edu','passe','Ronald');
-INSERT INTO UtilisateursEnregistres (email , motDePasse, prenom)
-VALUES ('MariannaRo@tub','passe','Rose');
-INSERT INTO UtilisateursEnregistres (email , motDePasse, prenom)
-VALUES ('Phil234@etu','passe','Phil');
-
-
 CREATE TABLE Don (
     montantDon FLOAT NOT NULL,
     dateDon DATE,
@@ -172,14 +91,6 @@ CREATE TABLE Don (
     CHECK (montantDon>0)
 );
 
-INSERT INTO Don (montantDon, dateDon, utilisateur) VALUES (0.1,'2000/01/13','Ron@edu');
-INSERT INTO Don (montantDon, dateDon, utilisateur) VALUES (0.2,'2000/01/13','Ron@edu');
-INSERT INTO Don (montantDon, dateDon, utilisateur) VALUES (0.1,'2000/01/14','Ron@edu');
---INSERT INTO Don (montantDon, dateDon, utilisateur) VALUES (0.1,'2000/01/13','Alfred@edu'); impossible car alfred n'est pas dans la relation utilisateur
-INSERT INTO Don (montantDon, dateDon, utilisateur) VALUES (0.1,'2000/01/13','Phil234@etu');
---INSERT INTO Don ( dateDon, utilisateur) VALUES ('2000/01/13','Phil234@etu'); une valeur NULL viole la contrainte NOT NULL de la colonne « montantdon » DETAIL: La ligne en échec contient (null, 2000-01-13, Phil234@etu)
---INSERT INTO Don (montantDon, utilisateur) VALUES (0.1,'Phil234@etu'); ERREUR: une valeur NULL viole la contrainte NOT NULL de la colonne « datedon » DETAIL: La ligne en échec contient (0.1, null, Phil234@etu)
-
 CREATE TABLE Abonnement (
     auteurNom VARCHAR(255),
     auteurPrenom VARCHAR(255),
@@ -187,12 +98,6 @@ CREATE TABLE Abonnement (
     PRIMARY KEY (auteurNom,auteurPrenom,utilisateur),
     FOREIGN KEY (auteurNom,auteurPrenom) REFERENCES Auteur(nom,prenom)
 );
-
-INSERT INTO Abonnement (auteurNom,auteurPrenom,utilisateur) VALUES ('Kevin','Integration','Phil234@etu');
-INSERT INTO Abonnement (auteurNom,auteurPrenom,utilisateur) VALUES ('Kevin','Integration','Ron@edu');
-INSERT INTO Abonnement (auteurNom,auteurPrenom,utilisateur) VALUES ('Kevin','Integration','MariannaRo@tub');
-INSERT INTO Abonnement (auteurNom,auteurPrenom,utilisateur) VALUES ('Kevin','Olivera','Phil234@etu');
---INSERT INTO Abonnement (auteurNom,utilisateur) VALUES ('Kevin','Phil234@etu'); La clé (auteurnom, auteurprenom)=(Kevin, Integration) n'est pas présente dans la table « auteur ».
 
 
 CREATE TABLE Aime (
