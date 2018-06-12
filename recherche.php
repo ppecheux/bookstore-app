395c8a7ad932b64b596e86a8c1219744b3603af9
NF17 Etudiant <nf17p165@smestaens24ab.sme.utc>
2018-06-12 16:22:36 +0200

    dubut ajout accueil.html

--- recherche.php
+++ recherche.php
@@ -6,29 +6,55 @@
 <body>
   <p>Voici les résultat de la recherche pour le mot clé <?php echo $_POST['motCle']?> : <p>
 
-  <table border="1">
-    <tr><th>titre</th><th>langue</th><th>page</th><th>resume</th></tr>
-	<?php
-	
+  <?php
+    $option = $_POST['option'];
+
     $dbUser = 'nf17p165';
     $userPw = 'wUANb2Da';
 		$vConn = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=dbnf17p165', $dbUser, $userPw);
-		
-		$vSql ="SELECT titre, langue, page, resume
-						FROM Livre
-						WHERE titre LIKE '%".$_POST['motCle']."%';";
-		$vQuery = $vConn->prepare($vSql);
-		$vQuery->execute();
-		while ($row = $vQuery->fetch(PDO::FETCH_ASSOC)) { 	
-				echo "<tr>";
-		  	echo "<td>$row[titre]</td>";
-		  	echo "<td>$row[langue]</td>";
-		  	echo "<td>$row[page]</td>";
-				echo "<td>$row[resume]</td>";
-		  	echo "</tr>";
-		}
+
+    if($option == "titre"){
+      echo '<table border="1">';
+      echo '<tr><th>titre</th><th>langue</th><th>page</th><th>resume</th></tr>';
+      $vSql ="SELECT titre, langue, page, resume
+              FROM Livre
+              WHERE titre LIKE '%".$_POST['motCle']."%';";
+      $vQuery = $vConn->prepare($vSql);
+      $vQuery->execute();
+      while ($row = $vQuery->fetch(PDO::FETCH_ASSOC)) {
+          echo "<tr>";
+          echo "<td>$row[titre]</td>";
+          echo "<td>$row[langue]</td>";
+          echo "<td>$row[page]</td>";
+          echo "<td>$row[resume]</td>";
+          echo "</tr>";
+      }
+    }
+    if($option == "auteur"){
+      echo '<table border="1">';
+      echo '<tr><th>titre</th><th>langue</th><th>page</th><th>resume</th>
+      <th>nom de l auteur</th><th>prenom de l auteur</th></tr>';
+      $vSql ="SELECT titre, langue, page, resume
+              FROM Livre
+              WHERE titre LIKE '%".$_POST['motCle']."%';";
+      $vQuery = $vConn->prepare($vSql);
+      $vQuery->execute();
+      while ($row = $vQuery->fetch(PDO::FETCH_ASSOC)) {
+          echo "<tr>";
+          echo "<td>$row[titre]</td>";
+          echo "<td>$row[langue]</td>";
+          echo "<td>$row[page]</td>";
+          echo "<td>$row[resume]</td>";
+          echo "<td>$row[nom]</td>";
+          echo "<td>$row[prenom]</td>";
+          echo "</tr>";
+      }
+    }
+
+
+
 		$vConn=NULL;
-		
+
 	?>
 	</table>
   <div align = "right"><a href="accueil.html">Retour à l'accueil</a></div>
