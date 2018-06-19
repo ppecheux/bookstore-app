03fb47ce3d8d0d386460f90100ed16bbac780730
pl <ppecheux@etu.utc.fr>
2018-06-19 15:28:42 +0200

    page des vedettes

---
+++ pageVedettes.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+  <title>Pirate Book</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<header>
+	<center><h1>pages des vedettes</h1></center>
+	<nav>
+			<ul>
+				<li><a href="accueil.html">Rechercher livre</a></li>
+				<li><a href="auteur.html">Rechercher auteur</a></li>
+				<li><a href="profilUtilisateur.php">Mon profil</a></li>
+				<li><a href="pageIdentificationUtilisateur.html">Se connecter</a></li>
+        <li><a href="deco.php">Se déconnecter</a></li>
+			<ul/>
+		</nav>
+</header>
+<body>
+  <?php
+
+
+    //connection à la base de donnée
+    $dbUser = 'nf17p165';
+    $userPw = 'wUANb2Da';
+    $vConn = new PDO('pgsql:host=tuxa.sme.utc;port=5432;dbname=dbnf17p165', $dbUser, $userPw);
+
+    $vSql ="SELECT * FROM vVedette;";
+
+    $vQuery = $vConn->prepare($vSql);
+    $vQuery->execute();
+
+    echo "<br><br>Livres : <br>";
+    echo '<table border="1">';
+    echo '<tr><th>titre</th><th>langue</th><th>page</th><th>resume</th></tr>';
+
+    while ($row = $vQuery->fetch(PDO::FETCH_ASSOC)) {
+      echo "<tr>";
+      echo "<td>$row[titre]</td>";
+      echo "<td>$row[langue]</td>";
+      echo "<td>$row[page]</td>";
+      echo "<td>$row[resume]</td>";
+      echo "<td>$row[categorie]</td>";
+      echo "<td>$row[licence]</td>";
+      echo "<td>$row[dateLimite]</td>";
+      echo "<td>$row[phraseAccroche]</td>";
+      echo "<td>$row[auteurNom]</td>";
+      echo "<td>$row[auteurPrenom]</td>";
+      echo "</tr>";
+    }
+    echo '</table>';
+
+    //déconnection de la base de donnée
+    $vConn=NULL;
+  ?>
+</body>
+</html>
