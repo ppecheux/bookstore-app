e195354856c536ea70be89d9a245dfe35ceff286
NF17 Etudiant <nf17p165@smestaens24ab.sme.utc>
2018-06-19 16:20:46 +0200

    rectification des titres

--- inscriptionUtilisateur.php
+++ inscriptionUtilisateur.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <header>
-	<center><h1>Bienvenue sur THE PIRATE BOOK !!!!!</h1></center>
+	<center><h1>S'inscrire</h1></center>
 	<nav>
 			<ul>
 				<li><a href="accueil.html">Rechercher livre</a></li>
@@ -13,6 +13,8 @@
 				<li><a href="profilUtilisateur.php">Mon profil</a></li>
 				<li><a href="pageIdentificationUtilisateur.html">Se connecter</a></li>
 				<li><a href="deco.php">Se déconnecter</a></li>
+				<li><a href="don.html">Faire un don</a></li>
+				<li><a href="pageVedettes.php">Livres en Vedette</a></li>
 			<ul/>
 		</nav>
 </header>
